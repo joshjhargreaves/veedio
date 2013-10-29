19b1db2d9c2845cb2cb49e9d9e09b81f3844ea37
Josh Hargreaves <joshyhargreaves@gmail.com>
2013-10-29 14:31:18 +0000

    corrected css links. Added api to add urls to database. Added routing for get req at urls

--- app.js
+++ app.js
@@ -9,14 +9,24 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var exphbs  = require('express3-handlebars');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db =  monk('localhost:27017/veedio');
 
 var app = express();
 
+hbs = exphbs.create({
+	defaultLayout: 'main',
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo: function () { return 'https://www.filepicker.io/api/file/e6Oz8XRvTWCJAUm6Rivd'; }
+    }
+});
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -35,12 +45,28 @@ app.get('/', function(request, response) {
    response.render('home');
 });
 
-app.get('/:id', function(request, response) {
-   response.send('The id is:' + request.params.id);
+app.get('/cdn/:id', function(request, response) {
+   var collection = db.get('urls');
+   collection.find({_id : request.params.id}, function(e,docs){
+		response.render('url', {
+   		helpers: {
+   			foo: function () { return docs[0].inkurl; }
+   		}
+   		});
+	})
 });
 
-app.get('/upload/:url', function(request, response) {
-   response.send('The id is:' + request.params.id);
+app.get('/collections',function(req,res){
+  db.driver.collectionNames(function(e,names){
+    res.json(names);
+  })
+});
+
+app.post('/upload', function(req, res) {
+	var collection = db.get('urls');
+	collection.insert({inkurl: req.body.url}, function(err,docsInserted){
+    	res.send(docsInserted);
+	});
 });
 
 app.get('/users', user.list);
